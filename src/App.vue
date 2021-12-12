380cdd598b089e4dcbaaec4e7b3c3b7d1f134f54
Renat Notfullin <renat@notfullin.com>
2021-12-12 19:59:50 +0300

    🌈 Improve top navbar

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,26 @@
 <template>
     <v-app>
-        <v-app-bar app color="primary" dark>
-            <div class="d-flex align-center">
-                <h1>Gout diet table</h1>
-            </div>
+        <v-app-bar app dark color="green" dense elevation="0">
+    
+            <v-toolbar-title>Low Purines diet table</v-toolbar-title>
 
             <v-spacer></v-spacer>
 
-            <v-btn
-                href="https://github.com/Renset/gout-diet"
-                target="_blank"
-                text
-            >
-                <span class="mr-2">GitHub</span>
-                <v-icon>mdi-open-in-new</v-icon>
-            </v-btn>
+            <v-toolbar-items>
+            
+                <v-select style="width: 120px; padding-top: 3px;" v-model="$i18n.locale" :items="langs" prepend-icon="mdi-translate" outlined dense>
+
+                </v-select>
+                <v-btn
+                    href="https://github.com/Renset/gout-diet"
+                    target="_blank"
+                    text
+                >
+                    <span class="mr-2">GitHub</span>
+                    <v-icon>mdi-open-in-new</v-icon>
+                </v-btn>
+            </v-toolbar-items>
+            
         </v-app-bar>
 
         <v-main>
@@ -39,6 +45,8 @@ export default {
         Footer
     },
 
-    data: () => ({}),
+    data: () => ({
+        langs: ['ru', 'en']
+    }),
 };
 </script>
